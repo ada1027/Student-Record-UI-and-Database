13587ec61200315ae0b47b65ad96e27db70d2921
ada <84153127+hungryhippo27@users.noreply.github.com>
2023-02-26 18:02:09 -0500

    Update studentEvent.py

--- studentEvent.py
+++ studentEvent.py
@@ -1,8 +1,8 @@
-class StudentEvent:
-    def __init__(self, studentId, eventDate, eventName, involvement, points, id = 0):
-        self.studentId = studentId
-        self.eventDate = eventDate
-        self.eventName = eventName
+class Student_event:
+    def __init__(self, student_id, event_date, event_name, involvement, points, id = 0):
+        self.student_id = student_id
+        self.event_date = event_date
+        self.event_name = event_name
         self.involvement = involvement
         self.points = points
         self.id = id

c78da235cfa4476b406c46eba324414a6359b7e3
ada <84153127+hungryhippo27@users.noreply.github.com>
2023-02-26 18:01:58 -0500

    Update student.py

--- student.py
+++ student.py
@@ -1,8 +1,8 @@
 import studentEvent as event
 class Student:
-    def __init__(self, firstName, lastName, grade, id, points):
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self, first_name, last_name, grade, id, points):
+        self.first_name = first_name
+        self.last_name = last_name
         self.grade = grade
         self.id = id
         self.totalPoints = points

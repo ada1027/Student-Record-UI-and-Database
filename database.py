c938d3e165c172b81338c71e5ee342bfc6ebc0f3
ada <84153127+hungryhippo27@users.noreply.github.com>
2023-02-26 18:01:45 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,57 +1,57 @@
 import sqlite3
 from student import Student
-from studentEvent import StudentEvent
+from studentEvent import Student_event
 
 class Database:
     def __init__(self):
-        self.conn = sqlite3.connect("studentEvents.db")
+        self.conn = sqlite3.connect("student_events.db")
         self.cur=self.conn.cursor()
         try:
-            studendTable = '''CREATE TABLE IF NOT EXISTS students (
+            student_table = '''CREATE TABLE IF NOT EXISTS students (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                firstname TEXT NOT NULL, 
-                lastname TEXT NOT NULL, 
+                first_name TEXT NOT NULL, 
+                last_name TEXT NOT NULL, 
                 grade INT NOT NULL
             );'''
-            self.conn.execute(studendTable)
-            eventsTable = '''CREATE TABLE IF NOT EXISTS student_events (
+            self.conn.execute(student_table)
+            events_table = '''CREATE TABLE IF NOT EXISTS student_events (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                studentid INTEGER NOT NULL,
-                eventdate TEXT NOT NULL, 
-                eventname TEXT NOT NULL, 
+                student_id INTEGER NOT NULL,
+                event_date TEXT NOT NULL, 
+                event_name TEXT NOT NULL, 
                 involvement TEXT NOT NULL, 
                 points INT NOT NULL DEFAULT 1,
-                FOREIGN KEY (studentid) REFERENCES students (id)
+                FOREIGN KEY (student_id) REFERENCES students (id)
             );'''
-            self.conn.execute(eventsTable)
+            self.conn.execute(events_table)
         except Exception as error:
             print ("database create error:", TabError)
 
-    def insertStudent(self, firstName, lastName, grade):
-        self.cur.execute("INSERT INTO students(firstname, lastname, grade) VALUES (?,?,?)",\
-                         (firstName, lastName, grade))
+    def insert_student(self, first_name, last_name, grade):
+        self.cur.execute("INSERT INTO students(first_name, last_name, grade) VALUES (?,?,?)",\
+                         (first_name, last_name, grade))
         self.conn.commit()
-        studentid = self.cur.lastrowid
-        print ("new id ", studentid)
-        return studentid
+        student_id = self.cur.lastrowid
+        print ("new id ", student_id)
+        return student_id
     
-    def insertEvent(self, studentId, eventDate, eventName, involvement, points):
-        self.cur.execute("INSERT INTO student_events(studentid, eventdate, eventname, involvement, points) VALUES (?,?,?,?,?)", \
-                         (studentId, eventDate, eventName, involvement, points))
+    def insert_event(self, student_id, event_date, event_name, involvement, points):
+        self.cur.execute("INSERT INTO student_events(student_id, event_date, event_name, involvement, points) VALUES (?,?,?,?,?)", \
+                         (student_id, event_date, event_name, involvement, points))
         self.conn.commit()
 
-    def updateEvent(self, id, studentId, eventDate, eventName, involvement, points):
-        self.cur.execute("UPDATE student_events SET studentid=?,eventdate=?,eventname=?,involvement=?,points=? WHERE id=?", \
-                         (studentId, eventDate, eventName, involvement, points, id))
+    def update_event(self, id, student_id, event_date, event_name, involvement, points):
+        self.cur.execute("UPDATE student_events SET student_id=?,event_date=?,event_name=?,involvement=?,points=? WHERE id=?", \
+                         (student_id, event_date, event_name, involvement, points, id))
         self.conn.commit()
 
-    def getAllStudents(self, grade = 0):
-        query = '''SELECT s.id, s.firstname, s.lastname, s.grade, 
+    def get_all_students(self, grade = 0):
+        query = '''SELECT s.id, s.first_name, s.last_name, s.grade, 
             (SELECT SUM(e.points)
                 FROM student_events e
-                WHERE s.id = e.studentId
+                WHERE s.id = e.student_id
             ) as points
             FROM students s'''
         if grade == 0:
@@ -61,20 +61,20 @@ class Database:
         self.cur.execute(query)
         return self.cur.fetchall()
     
-    def getStudentEvents(self, studentId):
-        query = '''SELECT id, studentid, eventdate, eventname, involvement, points
-            FROM student_events where studentid = {};'''.format(studentId)
+    def get_student_events(self, student_id):
+        query = '''SELECT id, student_id, event_date, event_name, involvement, points
+            FROM student_events where student_id = {};'''.format(student_id)
         self.cur.execute(query)
         return self.cur.fetchall()
 
-    def searchByName(self, name):
-        query = '''SELECT s.id, s.firstname, s.lastname, s.grade, 
+    def search_by_name(self, name):
+        query = '''SELECT s.id, s.first_name, s.last_name, s.grade, 
             (SELECT SUM(e.points)
                 FROM student_events e
-                WHERE s.id = e.studentId
+                WHERE s.id = e.student_id
             ) as points
             FROM students s
-            where s.firstname like '%{}%' OR s.lastname like '%{}%';'''.format(name, name)
+            where s.first_name like '%{}%' OR s.last_name like '%{}%';'''.format(name, name)
         self.cur.execute(query)
         return self.cur.fetchall()
 

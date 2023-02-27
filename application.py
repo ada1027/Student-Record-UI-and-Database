8bdeb7dc826d874cf8af0408daf564dd84876c3a
ada <84153127+hungryhippo27@users.noreply.github.com>
2023-02-26 21:58:12 -0500

    Update application.py

--- application.py
+++ application.py
@@ -10,31 +10,32 @@ import csv
 
 db = Database()
 selected_student_id = 0
-
+def clear_fields():
+    global selected_student_id
+    selected_student_id = 0
+    set_text(first_name_entry, "", True)
+    set_text(last_name_entry, "", True)
+    grade_combobox.config(state=NORMAL)
+    grade_combobox.set("")
+    new_student.config(state=NORMAL)
 def add_student(): # add_student button event handler
     global selected_student_id
-    if selected_student_id != 0:
-        selected_student_id = 0
-        set_text(first_name_entry, "", True)
-        set_text(last_name_entry, "", True)
-        grade_combobox.config(state=NORMAL)
-        grade_combobox.set("")
-    else:
-        first_name = first_name_entry.get()
-        last_name = last_name_entry.get()
-        if (first_name and last_name):
-            grade_value = grade_combobox.get()
-            if grade_value:
-                grade = grade_dict[grade_value] 
-                selected_student_id = db.insert_student(first_name, last_name, grade)
-                if selected_student_id > 0:
-                    first_name_entry.configure(state=DISABLED)
-                    last_name_entry.config(state=DISABLED)
-                    grade_combobox.config(state=DISABLED)
-            else:
-                messagebox.showwarning(title="Error", message="Please select a grade")
+    first_name = first_name_entry.get()
+    last_name = last_name_entry.get()
+    if (first_name and last_name):
+        grade_value = grade_combobox.get()
+        if grade_value:
+            grade = grade_dict[grade_value] 
+            selected_student_id = db.insert_student(first_name, last_name, grade)
+            if selected_student_id > 0:
+                new_student.config(state=DISABLED)
+                first_name_entry.configure(state=DISABLED)
+                last_name_entry.config(state=DISABLED)
+                grade_combobox.config(state=DISABLED)
         else:
-            messagebox.showwarning(title="Error", message="Not all fields filled.")
+            messagebox.showwarning(title="Error", message="Please select a grade")
+    else:
+        messagebox.showwarning(title="Error", message="Not all fields filled.")
 
 def add_event():
     global selected_student_id
@@ -58,7 +59,7 @@ def reset_event_fields():
     title_combobox.set("")
 
 def student_selected(event):
-    global selected_student_tupel
+    global selected_student_tuple
     selected_student_tuple = student_list_box.tree.item(student_list_box.tree.focus())["values"]
     if selected_student_tuple:
         # print(selected_student_tuple) # [2, 'first_name', 'last_name', 10, 100]
@@ -67,6 +68,7 @@ def student_selected(event):
         items = db.get_student_events(selected_student_id)
         event_item = map(lambda x: x[2:], items)
         event_list_box.show_items(event_item)
+        new_student.config(state=DISABLED)
         set_text(first_name_entry, selected_student_tuple[1], False)
         set_text(last_name_entry, selected_student_tuple[2], False)
         grade_combobox.current(selected_student_tuple[3] - 9)
@@ -98,6 +100,7 @@ def view_all_students():
     event_list_box.show_items([])
     global selected_student_id
     selected_student_id = 0
+    new_student.config(state=NORMAL)
 
 def get_grade_students():
     grade_value = grade_filter_combobox.get()
@@ -124,33 +127,20 @@ def export_grade_students():
                 writer.writerow(row[1:])
 
 window = tkinter.Tk()
-window.title("Data Entry Form")
-center = tkinter.Frame(window)
-center.pack(padx=20,pady=20,anchor=CENTER)
-frame = tkinter.Frame(center)
-frame.pack(padx=20, pady = 20, side = LEFT, fill = BOTH)
+window.title("Student Record Update")
 
-#Leaderboard
-leaderboard_frame = tkinter.Frame(center)
-leaderboard_frame.pack(padx=20, pady = 20, side=RIGHT, fill = BOTH)
+Grid.rowconfigure(window,0,weight=1)
+Grid.columnconfigure(window,0,weight=1)
+Grid.rowconfigure(window,1,weight=1)
 
-student_list_box = Sortable_list_box(ttk, leaderboard_frame, 
-    ["ID", "First Name", "Last Name", "Grade", "Total Points"])
-student_list_box.tree.bind('<ButtonRelease-1>', student_selected)
-event_list_box = Sortable_list_box(ttk, leaderboard_frame, 
-    ["Date", "Event", "Involvement", "Points"])
-
-#New Register or Search Student
-user_registration_frame = tkinter.LabelFrame(frame,width=10, height=200, text = " Student Registration ")
-user_registration_frame.grid(row= 0, column=0, sticky="news", padx=20, pady=5)
+frame = tkinter.Frame(window)
+frame.grid(row=0,column=0,sticky="NSEW")
 
 
-new_student = tkinter.Button(user_registration_frame, text="Add New Student", command=add_student)
-new_student.grid(row=0, column=0, sticky="news", padx=10, pady=10)
 
 # Saving User Info
 user_info_frame =tkinter.LabelFrame(frame, text=" Student Information ")
-user_info_frame.grid(row= 1, column=0, sticky = "news", padx=20, pady=5)
+user_info_frame.grid(row= 0, column=0, sticky = "news", padx=20, pady=5)
 
 first_name_label = tkinter.Label(user_info_frame, text="First Name")
 first_name_label.grid(row=0, column=0)
@@ -163,18 +153,24 @@ first_name_entry.grid(row=1, column=0,padx=20,pady=10)
 last_name_entry.grid(row=1, column=1,padx=20,pady=10)
 
 # grades
-grade_frame = tkinter.LabelFrame(frame, text = " Select Grade: ")
-grade_frame.grid(row=2, column=0, sticky="news", padx=20, pady=5)
-grade_label = tkinter.Label(grade_frame, text = "Select Grade: ")
-grade_dict = {"grade 9": 9, "grade 10": 10, "grade 11": 11, "grade 12": 12}
-grade_combobox = ttk.Combobox(grade_frame, state="readonly", values=list(grade_dict.keys()), width = 17)
-grade_label.grid(row=0, column=1,sticky="news",padx = 15)
-grade_combobox.grid(row=1, column=1,padx=20, pady = 10)
+grade_label = tkinter.Label(user_info_frame, text = "Select Grade: ")
+grade_dict = {"Grade 9": 9, "Grade 10": 10, "Grade 11": 11, "Grade 12": 12}
+grade_combobox = ttk.Combobox(user_info_frame, state="readonly", values=list(grade_dict.keys()), width = 17)
+grade_label.grid(row=2, column=0,sticky="news",padx = 15)
+grade_combobox.grid(row=3, column=0,padx=20, pady = 10)
+
+#Clear Fields
+clear_field = tkinter.Button(user_info_frame,text="Reset", command = clear_fields)
+clear_field.grid(row=3, column=1, sticky="news", padx=10, pady=10)
+
+#Register New Student
+new_student = tkinter.Button(user_info_frame, text="Add New Student", command=add_student)
+new_student.grid(row=4, column=0, columnspan=2, sticky="news", padx=10, pady=10)
 
 
 #event logs
 event_frame = tkinter.LabelFrame(frame, text = "Event Log: ")
-event_frame.grid(row=3, column = 0,sticky = "news",padx = 20, pady = 5)
+event_frame.grid(row=4, column = 0,sticky = "news",padx = 20, pady = 5)
 
 event_values = ["Basketball Game", "Baseball Game", "Volleyball Game", "Soccer Game", "Talent Show", "Christmas Concert", "Spring Concert", "Mass", "Prayer"]
 events_label = tkinter.Label(event_frame, text="Event: ")
@@ -198,28 +194,38 @@ points_spinbox = tkinter.Spinbox(event_frame, from_=1, to=1000,width=15)
 points_label.grid(row=3, column=1)
 points_spinbox.grid(row=4, column=1, padx=10, pady=10)
 
-# Button
 button = tkinter.Button(frame, text="Add Event", command=add_event)
 button.grid(row=5, column=0, sticky="news", padx=20, pady=10)
-view_all_button = tkinter.Button(frame, text="show all students", command=view_all_students)
-view_all_button.grid(row=6, column=0, sticky="news", padx=20, pady=10)
 
-search_button = tkinter.Button(frame, text = "Search by name", command=search_by_name)
-search_button.grid(row = 7, column = 0)
-search_input = tkinter.Entry(frame)
-search_input.grid(row = 7, column = 1)
+# Filters
+left_frame = tkinter.Frame(window)
+left_frame.grid(row = 0, column = 1, sticky= "NSEW")
 
-grade_filter_label = tkinter.Label(frame, text="Grade: ")
-grade_filter_combobox = ttk.Combobox(frame, state="readonly", values=list(grade_dict.keys()), width = 17)
-grade_filter_label.grid(row=8, column=0,sticky="news",padx = 15)
-grade_filter_combobox.grid(row=8, column=1,padx=20, pady = 10)
+search_button = tkinter.Button(left_frame, text = "Search by name", command=search_by_name)
+search_button.grid(row = 0, column = 2)
+search_input = tkinter.Entry(left_frame)
+search_input.grid(row = 0, column = 1, pady= 10)
+
+grade_filter_combobox = ttk.Combobox(left_frame, state="readonly", values=list(grade_dict.keys()), width = 17)
+grade_filter_combobox.grid(row=1, column=1)
 grade_filter_combobox.current(0)
 
-view_grade_button = tkinter.Button(frame, text = "View students in grade", command=view_grade_students)
-view_grade_button.grid(row = 9, column = 0)
+view_grade_button = tkinter.Button(left_frame, text = "View Students in Grade", command=view_grade_students)
+view_grade_button.grid(row = 1, column = 2,padx=10)
 
-export_grade_button = tkinter.Button(frame, text = "Export students in grade", command=export_grade_students)
-export_grade_button.grid(row = 9, column = 1)
+export_grade_button = tkinter.Button(left_frame, text = "Export Students in Grade", command=export_grade_students)
+export_grade_button.grid(row = 1, column = 0,padx=10)
+
+view_all_button = tkinter.Button(left_frame, text="Show All Students", command=view_all_students,width=0)
+view_all_button.grid(row = 0, column = 0)
+
+#Leaderboard
+leaderboard_frame = tkinter.Frame(left_frame)
+leaderboard_frame.grid(row=5,column=0,sticky="NSEW",pady=10,columnspan=3)
+
+student_list_box = Sortable_list_box(ttk, leaderboard_frame, ["ID", "First Name", "Last Name", "Grade", "Total Points"],8)
+student_list_box.tree.bind('<ButtonRelease-1>', student_selected)
+event_list_box = Sortable_list_box(ttk, leaderboard_frame, ["Date", "Event", "Involvement", "Points"],5)
 
 window.mainloop()
 

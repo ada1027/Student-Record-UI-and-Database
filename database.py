4e4e07c9c66b8beee1efa97c16b0590121e02d2a
ada <84153127+hungryhippo27@users.noreply.github.com>
2023-03-01 21:21:44 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.conn = sqlite3.connect("student_events.db")
+        self.conn = sqlite3.connect("student_events.db") #connect to database
         self.cur=self.conn.cursor()
         try:
             student_table = '''CREATE TABLE IF NOT EXISTS students (
@@ -11,8 +11,8 @@ class Database:
                 first_name TEXT NOT NULL, 
                 last_name TEXT NOT NULL, 
                 grade INT NOT NULL
-            );'''
-            self.conn.execute(student_table)
+            );''' #create a table for student info- Id, Name and Grade
+            self.conn.execute(student_table) 
             events_table = '''CREATE TABLE IF NOT EXISTS student_events (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -23,35 +23,39 @@ class Database:
                 involvement TEXT NOT NULL, 
                 points INT NOT NULL DEFAULT 1,
                 FOREIGN KEY (student_id) REFERENCES students (id)
-            );'''
-            self.conn.execute(events_table)
+            );''' #create table for event logs, Id reference to student
+            self.conn.execute(events_table) 
+        # exception
         except Exception as error:
             print ("database create error:", TabError)
 
+    # insert student into database
     def insert_student(self, first_name, last_name, grade):
+        #insert student name and grade into student table
         self.cur.execute("INSERT INTO students(first_name, last_name, grade) VALUES (?,?,?)",\
                          (first_name, last_name, grade))
         self.conn.commit()
+        #set student id
         student_id = self.cur.lastrowid
         return student_id
     
+    #insert event info into database
     def insert_event(self, student_id, event_date, quarter, event_name, involvement, points):
+        #insert student id, date, quarter, name of event, participation type, and points into event table
         self.cur.execute("INSERT INTO student_events(student_id, event_date, quarter, event_name, involvement, points) VALUES (?,?,?,?,?,?)", \
                          (student_id, event_date, quarter, event_name, involvement, points))
         self.conn.commit()
 
-    def update_event(self, id, student_id, event_date, quarter, event_name, involvement, points):
-        self.cur.execute("UPDATE student_events SET student_id=?,event_date=?,quarter=?,event_name=?,involvement=?,points=? WHERE id=?", \
-                         (student_id, event_date, quarter, event_name, involvement, points, id))
-        self.conn.commit()
-
+    # get all student info
     def get_all_students(self, grade = 0):
+        # add points to student's total points
         query = '''SELECT s.id, s.first_name, s.last_name, s.grade, 
             (SELECT SUM(e.points)
                 FROM student_events e
                 WHERE s.id = e.student_id
             ) as total_points
             FROM students s'''
+        #order students from most points to least
         if grade == 0:
             query += " ORDER BY total_points DESC;"
         else:
@@ -59,13 +63,17 @@ class Database:
         self.cur.execute(query)
         return self.cur.fetchall()
     
+    # get event logs of specific student
     def get_student_events(self, student_id):
+        #search all events with specific student id
         query = '''SELECT id, student_id, event_date, quarter, event_name, involvement, points
             FROM student_events where student_id = {};'''.format(student_id)
         self.cur.execute(query)
         return self.cur.fetchall()
 
+    # search a student's name to find their record
     def search_by_name(self, name):
+        # search first and last names for user-inputed characters
         query = '''SELECT s.id, s.first_name, s.last_name, s.grade, 
             (SELECT SUM(e.points)
                 FROM student_events e
@@ -76,24 +84,8 @@ class Database:
         self.cur.execute(query)
         return self.cur.fetchall()
 
+    #close
     def __del__(self):
         self.conn.close()
 
-    # def quarter(self,eventDate):
-    #     query="ALTER TABLE student_events ADD Quarter VARCHAR(100)"
-    #     self.cur.execute(query)
-    #     self.conn.commit()
-    #     print("NEW COLUMN ADDED..")
-
-    # def quarter(self, eventDate):
-    #     if '2022-09-00' < eventDate < '2022-11-15':
-    #         query = "INSERT INTO student_events (Quarter) VALUES ('1');"
-    #     if '2022-11-15' < eventDate < '2023-01-31':
-    #         query = "INSERT INTO student_events (Quarter) VALUES ('2');"
-    #     if '2023-01-31' < eventDate < '2023-04-15':
-    #         query = "INSERT INTO student_events (Quarter) VALUES ('3');"
-    #     if '2023-04-15' < eventDate < '2023-06-31':
-    #         query = "INSERT INTO student_events (Quarter) VALUES ('4');"
-    #     self.cur.execute(query)
-    #     self.conn.commit()
-
+    
